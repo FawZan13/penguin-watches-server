866e29449d98a71ce260770bbf784825109e67d0
FawZan13 <fawzanbinmokhtar@gmail.com>
2021-12-08 07:17:34 +0600

    mew

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
